1ea6ece06f1ea5501c57a3f559857111fe0f6693
Said BİÇER <saidbicer@gmail.com>
2022-04-22 06:11:20 +0300

    no message

--- app/src/main/java/com/said/androidartbooktesting/roomdb/Art.kt
+++ app/src/main/java/com/said/androidartbooktesting/roomdb/Art.kt
@@ -1,7 +1,9 @@
 package com.said.androidartbooktesting.roomdb
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "arts")
 data class Art(
     @PrimaryKey(autoGenerate = true)
     var id : Int,
