16b0866006cdd68cbc9da97ce83248061d822ca5
Said BİÇER <saidbicer@gmail.com>
2022-04-22 11:39:59 +0300

    no message

---
+++ app/src/main/java/com/said/androidartbooktesting/dependencyinjection/AppModule.kt
@@ -0,0 +1,40 @@
+package com.said.androidartbooktesting.dependencyinjection
+
+import android.content.Context
+import androidx.room.Room
+import com.said.androidartbooktesting.api.RetrofitAPI
+import com.said.androidartbooktesting.roomdb.MyDatabase
+import com.said.androidartbooktesting.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRoomDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, MyDatabase::class.java, "ArtBookDB"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: MyDatabase) = database.artDao()
+
+    @Singleton
+    @Provides
+    fun injectRetrofitAPI(): RetrofitAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(RetrofitAPI::class.java)
+    }
+}
